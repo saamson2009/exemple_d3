e2376e05dd7692be2e200be4d1d55b72d402d7fb
TheoGitGud <theo.gerritsen0703@gmail.com>
2021-03-23 18:12:38 +0100

    cercle

---
+++ script.js
@@ -0,0 +1,27 @@
+// créer un tableau de données
+let donnees = [30, 60, 120, 340]
+
+let corps = d3.select('body');
+// ajouter un canevas à la page
+let canevas = corps.append('svg')
+    .attr('width', 1000)
+    .attr('height', 1000);
+
+//  ajouter un cercle
+
+// canevas.append('circle')
+//     .attr('cx', 300)
+//     .attr('cy', 300)
+//     .attr('r', 40);
+
+// ajout autant de cercle que de données
+
+canevas.selectAll('circle')
+    .data(donnees)
+    .enter()
+    .append('circle')
+    // d pour données i pour index
+        .attr('cx', (d => d))
+        .attr('cy', 500)
+        .attr('r', (d,i) => 5+i*20)
+        .style('opacity', 0.5)
